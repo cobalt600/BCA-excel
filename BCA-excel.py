c217a8033cd93c34bd1d60a0aa106d853a3f7fd3
cobalt600 <cobalt60.gamma@gmail.com>
2022-01-09 19:24:37 +0000

    ほぼ完成した気がする

--- BCA-excel.py
+++ BCA-excel.py
@@ -10,52 +10,104 @@
 #絶妙のpandasだとやりたいことと乖離する気がしたので、書き直す
 
 import openpyxl
+from openpyxl.drawing.image import Image
+from openpyxl.styles.borders import Border, Side
+from openpyxl.styles import Font
+from openpyxl.styles.alignment import Alignment
+
+import zipfile
+import shutil
+import os
+
+#当該のxlsxをZIP形式のファイルとして開いてグラフ（image.png）を保存する
+xlsx_zip = zipfile.ZipFile("sample.xlsx")
+xlsx_zip.extract("xl/media/image1.png")
+xlsx_zip.close()
+
+#sample.xlsxを開いて、Result（結果をまとめるシート）を削除して作成
 filepath = 'sample.xlsx'
 wb = openpyxl.load_workbook(filepath)
-
+wb.remove(wb["Result"])
 ws_new = wb.create_sheet(index=0, title='Result')
-#sample.xlsxを開いて、結果をまとめるシート作成
 
-ws = wb["Linear regression fit"]
-ws2 = wb["Result"]
+#とりあえず必要なシートを定義、ws1に情報を集める
+ws1 = wb["Result"]
+ws2 = wb["Linear regression fit"]
+ws3 = wb["End point_1"]
 
+#Linear~のA3:A10のRun infoをコピー、ResultシートのA1からに貼り付ける
+#この書き方は良くない気がしている（行ごと取得するのがスマートなのであとで直す）
 for i in range(3,10):
-    copy = ws.cell(row = i, column = 1).value
-    #Linear~のA3:A10の情報をコピー
-    ws2.cell(row = i-2, column = 1, value = copy)
-    #Valueに指定した値を指定先の列に貼り付ける
+    copy = ws2.cell(row = i, column = 1).value
+    ws1.cell(row = i-2, column = 1, value = copy)
 i += 1
 
-for i in range(12,19):
-    for j in range(1,4):
-        copy = ws.cell(row = i, column = j).value
-        #Linear~のA12:C18の情報をコピー
-        ws2.cell(row = i-11, column = j+5, value = copy)
-        #Valueに指定した値を指定先の列に貼り付ける
+#Linear~のA12:C18のCurve infoをコピー、ResultシートのF1からに貼り付ける
+#この書き方は良くない気がしている（行ごと取得するのがスマートなのであとで直す）
+for i in range(15,19):
+    for j in range(2,4):
+        copy = ws2.cell(row = i, column = j).value
+        ws1.cell(row = i-11, column = j+4, value = copy)
 i += 1
 j += 1
 
+#End point_1のA48:M56のConc dataをコピー、ResultシートのA9からに貼り付ける
+#この書き方は良くない気がしている（行ごと取得するのがスマートなのであとで直す）
+for i in range(48,57):
+    for j in range(1,14):
+        copy = ws3.cell(row = i, column = j).value
+        ws1.cell(row = i-39, column = j, value = copy)
+i += 1
+j += 1
 
+#罫線を追加
+sheet = wb['Result']
+side1 = Side(style='thin', color='000000')
+border_aro = Border(top=side1, bottom=side1, left=side1, right=side1)
+
+for row in sheet['B10:M17']:
+    for cell in row:
+        cell.border = border_aro
+
+#プレートの番地を太字でセンタリングにする
+sheet = wb['Result']
+for row in ws1["A9:M9"]:
+    for cell in row:
+        cell.alignment = Alignment(horizontal="centerContinuous")
+        cell.font = Font(bold=True)
+
+for row in ws1["A10:A17"]:
+    for cell in row:
+        cell.alignment = Alignment(horizontal="centerContinuous")
+        cell.font = Font(bold=True)
+
+#最初に保存したグラフ（画像）を指定してリサイズして貼り付け
+img = Image('xl/media/image1.png')
+img.width = 330
+img.height = 110
+ws1.add_image(img, 'H2')
+
+#行の高さと幅の調整
+sheet.column_dimensions['A'].width = 3.5
+sheet.column_dimensions['B'].width = 7
+sheet.column_dimensions['C'].width = 7
+sheet.column_dimensions['D'].width = 7
+sheet.column_dimensions['E'].width = 7
+sheet.column_dimensions['F'].width = 7
+sheet.column_dimensions['G'].width = 7
+sheet.column_dimensions['H'].width = 7
+sheet.column_dimensions['I'].width = 7
+sheet.column_dimensions['J'].width = 7
+sheet.column_dimensions['K'].width = 7
+sheet.column_dimensions['L'].width = 7
+sheet.column_dimensions['M'].width = 7
+
+#印刷領域の指定（列が1枚に収まるように）と上書き保存
+ws1.page_setup.fitToWidth = 1
+ws1.page_setup.fitToHeight = 0
+ws1.sheet_properties.pageSetUpPr.fitToPage = True
 
 wb.save(filepath)
-#上書き保存
 
-#今日はここまでにする。後日データ部分のコピペを追加する
-
-
-"""
-#エクセルファイルを読み込む（とりあえずカレントディレクトリでいいや）
-import pandas as pd
-input_file_name = 'sample.xlsx'
-input_book = pd.ExcelFile(input_file_name)
-
-#シートの名称は不変だけど、とりあえず取得
-input_sheet_name = input_book.sheet_names
-num_sheet = len(input_sheet_name)
-
-#シートの名前と総数を表示してみる
-print ("Sheet の数:", num_sheet)
-print (input_sheet_name)
-
-#ここから先は後で書く
-"""
+#最初に作られたImage.pngが保存されているディレクトリを削除する
+shutil.rmtree('xl/')
